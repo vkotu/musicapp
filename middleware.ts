b320f0c558e04a6162526619f80a7351331eb018
vkotu <vkotu@paypal.com>
2023-01-09 19:42:49 -0800

    Music app made with nextjs

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+
+const signedinPages = ['/', '/playlist', '/library']
+
+export default function middleware(req) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.get('TRAX_ACCESS_TOKEN')
+    if (!token) {
+      return NextResponse.rewrite(new URL('/signin', req.url))
+    }
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,82 @@
+import { Box, Text, Flex } from '@chakra-ui/layout'
+import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import GradientLayout from '../components/gradientLayout'
+import { useMe } from '../lib/hooks'
+import prisma from '../lib/prisma'
+import { validateToken } from '../lib/auth'
+
+const Home = ({ artists }) => {
+  const { user, isLoading } = useMe()
+  // const [test, setTest] = useState('kotu')
+  // if (!user) {
+  //   console.log('server side')
+  //   router.push('/signin')
+  // }
+  return (
+    <Box height="100%">
+      <Skeleton height="100%" isLoaded={!isLoading}>
+        <GradientLayout
+          roundImage
+          color="gray"
+          subtitle="profile"
+          title={`${user?.firstName} ${user?.lastName}`}
+          description={`${user?.playlistsCount} public playlists`}
+          image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+        >
+          <Box color="white" paddingX="40px">
+            <Box marginBottom="40px">
+              <Text fontSize="2xl" fontWeight="bold">
+                Top artist this month
+              </Text>
+              <Text fontSize="md">only visible to you</Text>
+            </Box>
+            <Flex>
+              {artists.map((artist) => (
+                <Box paddingX="10px" width="20%">
+                  <Box
+                    bg="gray.900"
+                    borderRadius="4px"
+                    padding="15px"
+                    width="100%"
+                  >
+                    <Image
+                      src="https://placekitten.com/300/300"
+                      borderRadius="100%"
+                    />
+                    <Box marginTop="20px">
+                      <Text fontSize="large">{artist.name}</Text>
+                      <Text fontSize="x-small">Artist</Text>
+                    </Box>
+                  </Box>
+                </Box>
+              ))}
+            </Flex>
+          </Box>
+        </GradientLayout>
+      </Skeleton>
+    </Box>
+  )
+}
+
+export const getServerSideProps = async ({ req }) => {
+  try {
+    validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+  } catch (e) {
+    console.log('redirecting in index..tsx')
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/signin',
+      },
+    }
+  }
+
+  const artists = await prisma.artist.findMany({})
+
+  return {
+    props: { artists },
+  }
+}
+
+export default Home
